68f423bf3633fb419014f0781a22d1f3e5df301a
dav010203 <davidsimonyan460@gmail.com>
2022-06-19 00:17:30 -0800

    Add 'name' attribute and change __repr__ of class 'Playlist'

--- src/player/playlist.py
+++ src/player/playlist.py
@@ -1,20 +1,21 @@
 from song import Song
 
 class Playlist:
-    def __init__(self) -> None:
-        self.playlist = []
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.songs = []
 
-    def load_songs(self, playlist_f):
-        with open(playlist_f, "r") as songs:
-            for s in songs:
+    def load_songs(self, file_name):
+        with open(file_name, "r") as songs_file:
+            print()
+            for s in songs_file:
                 s = s.split("\t")
                 song = Song(s[0], s[1], s[2], s[3].strip())
-                self.playlist.append(song)
-
+                self.songs.append(song)
     
     def __repr__(self) -> str:
-        string = ""
-        for song in self.playlist:
-            string += song.name + ", "
-        return string[:-2]
-
+        return self.name
+    
+p = Playlist("Favourites")
+p.load_songs("albums.txt")
+print(p.songs)
