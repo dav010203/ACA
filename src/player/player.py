8795e0b4375fc93353d04bf62071db9cdd81a297
dav010203 <davidsimonyan460@gmail.com>
2022-06-19 01:30:43 -0800

    Add '__repr__' and  other methods to class 'Player'

--- src/player/player.py
+++ src/player/player.py
@@ -2,4 +2,53 @@ from playlist import Playlist
 
 class Player:
     def __init__(self, playlist: Playlist) -> None:
+        self.power = False
         self.playlist = playlist
+        self.song_N = 0
+    
+    def play(self) -> None:
+        if self.power == False:
+            self.power = True
+        else:
+            print("It is already turned on!")
+
+    def stop(self) -> None:
+        if self.power == True:
+            self.power = False
+        else:
+            print("It is already turned off!")
+        pass
+
+    def next_song(self) -> None:
+        if self.power:
+            self.song_N += 1
+            if self.song_N > len(self.playlist.songs) - 1:
+                self.song_N = 0
+        else:
+            print("At first, turn on the player")
+
+    def previous_song(self) -> None:
+        if self.power:
+            self.song_N -= 1
+            if self.song_N < 0 :
+                self.song_N = len(self.playlist.songs) - 1
+        else:
+            print("At first, turn on the player")
+
+    def show_current_song(self):
+        return self.playlist.songs[self.song_N]
+
+    def __repr__(self) -> str:
+        return "Current - {}".format(self.show_current_song())
+
+plist = Playlist()
+plist.load_songs('albums.txt')
+
+pl = Player(plist)
+pl.play()
+pl.previous_song()
+pl.next_song()
+
+print(pl)
+
+    
