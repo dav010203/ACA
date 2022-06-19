f3e3a7b9b77c0e505f65d347f3b88047346135ea
dav010203 <davidsimonyan460@gmail.com>
2022-06-19 01:44:31 -0800

    Add if else in __repr__

--- src/player/player.py
+++ src/player/player.py
@@ -4,7 +4,7 @@ class Player:
     def __init__(self, playlist: Playlist) -> None:
         self.power = False
         self.playlist = playlist
-        self.song_N = 0
+        self.__song_N = 0
     
     def play(self) -> None:
         if self.power == False:
@@ -21,25 +21,29 @@ class Player:
 
     def next_song(self) -> None:
         if self.power:
-            self.song_N += 1
-            if self.song_N > len(self.playlist.songs) - 1:
-                self.song_N = 0
+            self.__song_N += 1
+            if self.__song_N > len(self.playlist.songs) - 1:
+                self.__song_N = 0
         else:
             print("At first, turn on the player")
 
     def previous_song(self) -> None:
         if self.power:
-            self.song_N -= 1
-            if self.song_N < 0 :
-                self.song_N = len(self.playlist.songs) - 1
+            self.__song_N -= 1
+            if self.__song_N < 0 :
+                self.__song_N = len(self.playlist.songs) - 1
         else:
             print("At first, turn on the player")
 
-    def show_current_song(self):
-        return self.playlist.songs[self.song_N]
+    def __show_current_song(self):
+        return self.playlist.songs[self.__song_N]
 
     def __repr__(self) -> str:
-        return "Current - {}".format(self.show_current_song())
+        if self.power == False:
+            return "The player is turned off"
+        else:
+            return "Current song is --> ({})".format(self.__show_current_song())
+
 
 plist = Playlist()
 plist.load_songs('albums.txt')
@@ -50,5 +54,3 @@ pl.previous_song()
 pl.next_song()
 
 print(pl)
-
-    
