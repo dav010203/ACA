9faa53d2c504f9a43718b6e49a99f000466c674a
dav010203 <davidsimonyan460@gmail.com>
2022-06-19 00:16:23 -0800

    Change __repr__ of class 'Song'

--- src/player/song.py
+++ src/player/song.py
@@ -1,9 +1,9 @@
 class Song:
-    def __init__(self, artist, album, year, name) -> None:
+    def __init__(self, artist: str, album: str, year: int, name: str) -> None:
         self.artist = artist
         self.album = album
         self.year = year
         self.name = name
 
     def __repr__(self):
-        return "Song: '{}' - Album: '{}' - '{}', {}".format(self.name, self.album, self.artist, self.year)
+        return "({} - {})".format(self.name, self.artist)
