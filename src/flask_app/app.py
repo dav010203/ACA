34e4fb6b2ddcb42b967a5bd945adbc2f5bbb8974
dav010203 <davidsimonyan460@gmail.com>
2022-07-27 07:23:13 -0800

    First commit

---
+++ src/flask_app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Worldd!</p>"
+
+from markupsafe import escape
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
